62372c004aaa2b2fc31ec41afde3a5ec27828deb
Nick JDA <armstrongnick1999@gmail.com>
2023-05-30 20:45:05 -0400

    Added max-length inquire plugin, added validate-color npm package, made questions const array and exported

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const QUESTIONS = require('./lib/questions');
+const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+const question = require('./lib/questions');
+const { Circle, Square, Triangle } = require('./lib/shapes');
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
-const questions = new QUESTIONS();
-
-questions.run();
+question.run();
