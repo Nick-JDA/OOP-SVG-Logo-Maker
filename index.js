64f3dc317f8c495342c0ed8f5368d0c3c1202808
Nick JDA <armstrongnick1999@gmail.com>
2023-05-30 21:53:14 -0400

    added variables/imports and writefile function to index.js, added package-lock.json to gitignore, adjusted code in test shapes.js, and questions to be DRY

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const inquirer = require('inquirer');
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
-const question = require('./lib/questions');
+const questions = require('./lib/questions');
 const { Circle, Square, Triangle } = require('./lib/shapes');
+const fs = require('fs');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
 
-question.run();
+function writeToFile() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(data)
+        let logoShape;
+        switch (data.shapeType) {
+            case 'circle':
+                logoShape = new Circle(data.logoText, data.textColor, data.shapeColor)
+                break;
+            case 'triangle':
+                logoShape = new Triangle(data.logoText, data.textColor, data.shapeColor)
+                break;
+            case 'square':
+                logoShape = new Square(data.logoText, data.textColor, data.shapeColor)
+                break;
+        }
+        let logoFinal = logoShape.render()
+        fs.writeFile("./examples/logo.svg", logoFinal, (error) => {
+            error ? console.log('error') : console.log('Logo Generated!');
+        })
+    })
+}
+writeToFile();
